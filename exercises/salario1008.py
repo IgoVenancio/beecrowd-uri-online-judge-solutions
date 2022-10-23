6f98f60b9ae39692e6f2eee40cd12285db314021
Igo <venancioigo@gmail.com>
2022-10-23 16:06:45 -0300

    1008 - Salário

---
+++ exercises/salario1008.py
@@ -0,0 +1,7 @@
+numero_funcionario = int(input())
+qtd_horas_trabalhadas = int(input())
+valor_horas_trabalhadas = float(input())
+salario = qtd_horas_trabalhadas * valor_horas_trabalhadas
+
+print(f"NUMBER = {numero_funcionario}")
+print(f"SALARY = U$ {salario:.2f}")
