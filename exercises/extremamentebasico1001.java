4622282620b24d70d41ad676e7384b0a68a8fdcb
IgoVenancio <57968259+IgoVenancio@users.noreply.github.com>
2022-11-23 12:05:48 -0300

    Create extremamentebasico1001.java

---
+++ exercises/extremamentebasico1001.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        int variable1 = scanner.nextInt();
+        int variable2 = scanner.nextInt();
+        int result = variable1 + variable2;
+        System.out.println("X = "+ result);
+ 
+    }
+ 
+}
