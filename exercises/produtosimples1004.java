6ea5551e6a23382bfe99e486d3eec71fee6ae9d6
IgoVenancio <57968259+IgoVenancio@users.noreply.github.com>
2022-11-23 12:13:48 -0300

    Create produtosimples1004.java

---
+++ exercises/produtosimples1004.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int A = ler.nextInt();
+		int B = ler.nextInt();
+		int PROD = A * B;
+		
+		System.out.println("PROD = "+PROD);
+	}
+
+}
