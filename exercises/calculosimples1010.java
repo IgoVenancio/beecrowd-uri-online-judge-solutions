9979431ed90def06347291c6695ab8cd43ba2762
IgoVenancio <57968259+IgoVenancio@users.noreply.github.com>
2022-11-23 13:16:28 -0300

    Create calculosimples1010.java

---
+++ exercises/calculosimples1010.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.*;
+import static java.lang.System.in;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(in);
+
+        String entrada1 = scanner.nextLine();
+        String entrada2 = scanner.nextLine();
+        String[] textoSeparado1 = entrada1.split(" ");
+        String[] textoSeparado2 = entrada2.split(" ");
+
+        double valorPagar1 = (Integer.parseInt(textoSeparado1[1]) * Double.parseDouble(textoSeparado1[2]));
+        double valorPagar2 = (Integer.parseInt(textoSeparado2[1]) * Double.parseDouble(textoSeparado2[2]));
+        double somaValorPagar = valorPagar1 + valorPagar2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f",somaValorPagar);
+        skipLine();
+ 
+        
+    }
+  
+    static void skipLine(){
+        System.out.println();
+    }
+  
+}
