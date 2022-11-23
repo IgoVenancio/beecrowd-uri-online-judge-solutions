f6976bd63849f3a864effbd28a85b8c792cadb9d
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2022-11-23 17:51:45 -0300

    Create gatodecombustivel1017.java

---
+++ exercises/gatodecombustivel1017.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+            Scanner scanner = new Scanner(System.in);
+            int consumoPorLitro = 12;
+
+            int tempo = scanner.nextInt();;
+            int velocidadeMedia = scanner.nextInt();
+
+            double litros = (double)(tempo * velocidadeMedia) /consumoPorLitro;
+
+            System.out.println(String.format("%.3f",litros));
+ 
+    
+    }
+
+    
+}
