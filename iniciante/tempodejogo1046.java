9d1373d676804f470d538cd4a16a2427b4347fb1
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2022-11-30 18:12:29 -0300

    Create tempodejogo1046.java

---
+++ iniciante/tempodejogo1046.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.*;
+
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+        int horaInicial = scanner.nextInt();
+        int horaFinal = scanner.nextInt();
+
+        System.out.println(String.format("O JOGO DUROU %d HORA(S)",duracaoJogo(horaInicial, horaFinal)));
+ 
+    }
+    
+    static int duracaoJogo(int a, int b){
+        if (a == b)
+            return 24;
+        if (a > b)
+            return 24 - a + b;
+        if (a < b)
+            return b - a;
+        return -1;
+    }
+ 
+}
