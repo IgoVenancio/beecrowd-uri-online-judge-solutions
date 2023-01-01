ebdca261574a48537b621c70db4cee9ec1246725
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-01 11:58:12 -0300

    Create formuladebhaskara1036.js

---
+++ iniciante/formuladebhaskara1036.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let entradas = lines.shift().split(' ')
+let a = parseFloat(entradas[0])
+let b = parseFloat(entradas[1])
+let c = parseFloat(entradas[2])
+
+let delta = Math.pow(b, 2) - 4 * a * c
+let r1 = (-b + Math.sqrt(delta))/(2 * a)
+let r2 = (-b - Math.sqrt(delta))/(2 * a)
+
+if(a == 0 || delta < 0){
+    console.log(`Impossivel calcular`)
+}else{
+    console.log(`R1 = ${r1.toFixed(5)}`)
+    console.log(`R2 = ${r2.toFixed(5)}`)
+}
