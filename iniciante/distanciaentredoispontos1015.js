b59c43426060a98625f180f04330336fa497da88
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2022-12-31 21:12:55 -0300

    Create distanciaentredoispontos1015.js

---
+++ iniciante/distanciaentredoispontos1015.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function distanciaEntreDoisPontos(a, b, c, d){
+    return Math.sqrt(Math.pow((c - a), 2) + Math.pow((d - b), 2))
+}
+
+let entrada1 = lines.shift().split(' ')
+let entrada2 = lines.shift().split(' ')
+
+console.log(`${distanciaEntreDoisPontos(parseFloat(entrada1[0]), parseFloat(entrada1[1]), parseFloat(entrada2[0]), parseFloat(entrada2[1])).toFixed(4)}`)
