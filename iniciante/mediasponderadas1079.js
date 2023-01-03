7e6ecd82f7872ba7ad018c44adf091016af29e7d
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-03 20:09:01 -0300

    Create mediasponderadas1079.js

---
+++ iniciante/mediasponderadas1079.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let entrada = parseInt(lines.shift())
+let countEntradas = 0
+
+while(countEntradas++ < entrada){
+    let valores = lines.shift().split(' ')
+    let mediaPonderada = (parseFloat(valores[0]) * 2 + parseFloat(valores[1]) * 3 + parseFloat(valores[2].replace('\r', '')) * 5)/10
+    console.log(mediaPonderada.toFixed(1))
+}
