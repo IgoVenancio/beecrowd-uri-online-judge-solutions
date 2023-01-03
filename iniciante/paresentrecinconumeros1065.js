c01c0b697337d233738c11b996cc0c55743544d9
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-03 18:00:56 -0300

    Create paresentrecinconumeros1065.js

---
+++ iniciante/paresentrecinconumeros1065.js
@@ -0,0 +1,14 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let countEntradas = 0
+let countPares = 0
+
+while(countEntradas++ < 5){
+    let entrada = parseInt(lines.shift())
+    if(entrada % 2 ===0){
+        countPares++
+    }
+}
+
+console.log(`${countPares} valores pares`)
