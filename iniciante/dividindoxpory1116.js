92bd3f2022bb1b4b0bd6fae70ffe63831ff26e36
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-04 17:17:43 -0300

    Create dividindoxpory1116.js

---
+++ iniciante/dividindoxpory1116.js
@@ -0,0 +1,22 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function divisao(a, b){
+    if(b !== 0){
+        console.log((a/b).toFixed(1))
+        return 1
+    }else{
+        console.log('divisao impossivel')
+        return 0
+    }
+}
+
+let entrada = parseInt(lines.shift())
+let count = 0
+
+while(count++ < entrada){
+    let entradas = lines.shift().split(' ')
+    let a = parseInt(entradas[0])
+    let b = parseInt(entradas[1].replace('\r',''))
+    divisao(a,b)
+}
