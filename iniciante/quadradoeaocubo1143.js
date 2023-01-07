3186b94929709b53e309f19c8b308846032a95e1
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-06 22:15:07 -0300

    Update quadradoeaocubo1143.js

--- iniciante/quadradoeaocubo1143.js
+++ iniciante/quadradoeaocubo1143.js
@@ -1,31 +1,8 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-let entrada = parseInt(lines.shift().replace('\r',''))
+let entrada = parseInt(lines.shift());
 
-let saida = []
-let count = 0
-let countEntrada = 0
-let j
-let z
-let i
-
-for(i = 1, z = 1, j = 0 ;countEntrada < entrada; ){
-
-    if(count++ < 3){
-        if(count == 1){
-            saida[j++] = z
-            i = z
-        }else{
-            saida[j] = saida[j - 1] * i
-            i = saida[j - 1]
-            j++
-        }
-    }else{
-        z++
-        count = 0
-        countEntrada++
-        j = 0
-        console.log(saida.join(' '))
-    }
+for(let i = 1; i <= entrada; i++){
+    console.log((i) + ' ' + (Math.pow(i,2)) + ' ' + (Math.pow(i,3)));
 }
