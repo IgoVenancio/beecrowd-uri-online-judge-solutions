5ddd2383b295bfc41eb60ad3b653cf4ef4b25dd2
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-08 20:05:31 -0300

    Create idades1154.js

---
+++ iniciante/idades1154.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let idade
+let somaIdades = 0
+let countIdades = 0
+
+do{
+    idade = parseFloat(lines.shift())
+    if(idade >= 0){
+        somaIdades += idade
+        countIdades++ 
+    } 
+    
+}while(idade > 0)
+
+console.log(`${(somaIdades/countIdades).toFixed(2)}`)
