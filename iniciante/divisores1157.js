002426a81132b463fce30b56ca3b756afedd64ba
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-08 20:26:35 -0300

    Create divisores1157.js

---
+++ iniciante/divisores1157.js
@@ -0,0 +1,13 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function divisores(a){
+    for(let i = 1; i <= a; i++){
+        if(a % i === 0)
+            console.log(i)
+    }
+}
+
+let x = parseInt(lines.shift())
+
+divisores(x)
