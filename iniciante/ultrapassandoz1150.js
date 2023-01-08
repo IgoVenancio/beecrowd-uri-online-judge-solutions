32801b7df7cc89152061078cdc6df4ac845635e7
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-08 18:22:29 -0300

    Create ultrapassandoz1150.js

---
+++ iniciante/ultrapassandoz1150.js
@@ -0,0 +1,19 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let x = parseInt(lines.shift())
+let z = parseInt(lines.shift())
+let i = 0
+let soma = 0
+
+while(z <= x){
+    z = parseInt(lines.shift())
+}
+
+do{
+    i++
+    soma += x
+    x++
+}while(soma < z)
+
+console.log(i)
