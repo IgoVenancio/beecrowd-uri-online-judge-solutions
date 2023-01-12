303e4f17edaecc6bbf80eacfc56a7a114e8bd0ba
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-01-12 16:13:33 -0300

    Create abaixodadiagonalprincipal1184.js

---
+++ iniciante/abaixodadiagonalprincipal1184.js
@@ -0,0 +1,44 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+function operacaoMatriz(matriz, operacao, qtdLinhas, qtdColunas){
+    let soma = 0
+    let media = 0
+    let countMedias = 0
+    if(operacao === 'S'){
+        for(let i = 1; i < qtdColunas; i++ ){
+            for(let j = 0; j < i; j++)
+                soma += matriz[i][j]
+        }
+        return soma
+        }
+
+    if(operacao === 'M'){
+        for(let i = 0; i < qtdColunas; i++ ){
+            for(let j = 0; j < i; j++){
+                media += matriz[i][j]
+                countMedias++
+            }
+                
+        }
+        return media /= countMedias
+    }
+
+}
+
+let linhas = 12
+let colunas = 12
+let matriz = new Array(colunas)
+
+for(let i = 0; i < colunas; i++)
+    matriz[i] = new Array(linhas)
+
+let operacao = lines.shift().replace('\r','')
+
+for(let i = 0; i < colunas; i++){
+    for(let j = 0; j < linhas; j++){
+        matriz[i][j] = parseFloat(lines.shift().replace('\r',''))
+    }
+}
+
+console.log(operacaoMatriz(matriz,operacao,linhas,colunas).toFixed(1))
