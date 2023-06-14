9bbe03b9f182c7a4e059014ac9e97cb0d21f8714
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-06-14 16:22:19 -0300

    Create 1037.js

---
+++ iniciante/javascript/1037.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const intervalo = function (a){
+    if(a >= 0 && a <= 25){
+        return 'Intervalo [0,25]'
+    }else if (a > 25 && a <= 50){
+        return 'Intervalo (25,50]'
+    }else if (a > 50 && a <= 75){
+        return 'Intervalo (50,75]'
+    }else if (a > 75 && a <= 100){
+        return 'Intervalo (75,100]'
+    }else{
+        return 'Fora de intervalo'
+    }
+
+}
+
+let entrada = parseFloat(lines.shift())
+
+console.log(intervalo(entrada))
