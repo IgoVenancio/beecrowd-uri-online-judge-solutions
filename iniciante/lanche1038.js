1823e2bd264152d18131d966ef85d5cc315b30bd
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-06-14 20:47:48 -0300

    Delete lanche1038.js

--- iniciante/lanche1038.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let entrada = lines.shift().split(' ')
-let valores = [0.0, 4.00, 4.50, 5.00, 2.00, 1.50]
-
-console.log(`Total: R$ ${(valores[parseInt(entrada[0])] * entrada[1]).toFixed(2)}`)
