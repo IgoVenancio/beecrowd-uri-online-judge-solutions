e2ba0b05d63361171f2a24349bc492a5df078eab
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-06-14 16:25:50 -0300

    Delete calculosimples1010.js

--- iniciante/calculosimples1010.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let entrada1 = lines.shift().split(' ')
-let entrada2 = lines.shift().split(' ')
-
-const valorAPagar = (array1, array2) => (Number(array1[1]) * Number(array1[2]) + Number(array2[1]) * Number(array2[2]))
-
-console.log(`VALOR A PAGAR: R$ ${valorAPagar(entrada1, entrada2).toFixed(2)}`)
