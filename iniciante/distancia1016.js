87ffcac9da0c36608f0b4546f9656ad8ce0cabdb
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-06-14 16:28:49 -0300

    Delete distancia1016.js

--- iniciante/distancia1016.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let tempo = parseInt(lines.shift())
-const calcularTempo = a => a * 2
-
-console.log(`${calcularTempo(tempo)} minutos`)
