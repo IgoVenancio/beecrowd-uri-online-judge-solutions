4e8f528121ad6931f6bd2fe4a747a8cc3901e565
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-11-21 19:01:55 -0300

    Create 1014.py

---
+++ iniciante/python/1014.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+distancia_percorrida = int(input())
+combustivel_utilizado = float(input())
+
+consumo_medio = distancia_percorrida / combustivel_utilizado
+
+print(f'{consumo_medio:.3f} km/l')
