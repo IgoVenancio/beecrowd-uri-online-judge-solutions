406028a0ce4242e285fb6983d27b1fcf6d4cb530
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-11-21 21:35:35 -0300

    Delete exercises directory

--- exercises/salario1008.py
+++
@@ -1,7 +0,0 @@
-numero_funcionario = int(input())
-qtd_horas_trabalhadas = int(input())
-valor_horas_trabalhadas = float(input())
-salario = qtd_horas_trabalhadas * valor_horas_trabalhadas
-
-print(f"NUMBER = {numero_funcionario}")
-print(f"SALARY = U$ {salario:.2f}")
