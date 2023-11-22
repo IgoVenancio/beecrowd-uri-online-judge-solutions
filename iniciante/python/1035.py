a14b7cf218ca2b5f3b749befab1ab2ea10a7ee47
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-11-22 10:16:40 -0300

    Create 1035.py

---
+++ iniciante/python/1035.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+entrada = input().split(' ')
+
+a = int(entrada[0])
+b = int(entrada[1])
+c = int(entrada[2])
+d = int(entrada[3])
+
+if b > c and \
+   d > a and \
+   (c + d) > (a + b) and \
+   c and \
+   d and \
+   (a % 2 == 0):
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
