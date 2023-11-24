88ee967298e32c0df0905f6345258db51f7d493c
Igo Venâncio <57968259+IgoVenancio@users.noreply.github.com>
2023-11-24 12:42:41 -0300

    Create 1052.py

---
+++ iniciante/python/1052.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+entrada = int(input())
+
+meses = ['January',
+         'February',
+         'March',
+         'April',
+         'May',
+         'June',
+         'July',
+         'August',
+         'September',
+         'October',
+         'November',
+         'December']
+
+print(f'{meses[entrada - 1]}')
