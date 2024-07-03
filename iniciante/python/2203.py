63162141d64ddb34289e02c52f432a84ce040e08
V <57968259+IgoVenancio@users.noreply.github.com>
2024-07-03 12:15:09 -0300

    Create 2203.py

---
+++ iniciante/python/2203.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        entrada = input().split()
+        entrada = [int(x) for x in entrada]
+
+        # distancia ab = raiz((x2 - x1)^2 + (y2 - y1)^2)
+        distancia = ((entrada[2] - entrada[0]) ** 2 +
+                     (entrada[3] - entrada[1]) ** 2) ** (1/2)
+        # R1 + R2 >= Distancia inicial + distancia final
+        if entrada[5] + entrada[6] >= distancia + entrada[4] * 1.5:
+            print('Y')
+        else:
+            print('N')
+    except EOFError:
+        break
