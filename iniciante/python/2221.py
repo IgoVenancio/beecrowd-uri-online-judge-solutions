2015040379d7aaf3a19b4bfa0095e21c8c231ac4
V <57968259+IgoVenancio@users.noreply.github.com>
2024-07-03 18:08:39 -0300

    Create 2221.py

---
+++ iniciante/python/2221.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def valor_golpe(a, b, c, bonus):
+    return (int(a) + int(b)) / 2 + (0, int(bonus))[int(c) % 2 == 0]
+
+
+qtd_entrada = int(input())
+
+while True:
+    bonus = int(input())
+    d_ai, d_di, d_li = input().split()
+    g_ai, g_di, g_li = input().split()
+    pomekon_dabriel = valor_golpe(d_ai, d_di, d_li, bonus)
+    pomekon_guarte = valor_golpe(g_ai, g_di, g_li, bonus)
+
+    if pomekon_dabriel > pomekon_guarte:
+        print('Dabriel')
+    elif pomekon_dabriel < pomekon_guarte:
+        print('Guarte')
+    else:
+        print('Empate')
+    qtd_entrada -= 1
+    if not qtd_entrada:
+        break
